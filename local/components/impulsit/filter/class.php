87589ab74026b6effb2aeb1701c6a88b4dce4f3d
Egor Pokalyuk <pokalukegor54@gmail.com>
2025-07-11 03:43:34 +0300

    Поправил вывод элементов фильтра

--- local/components/impulsit/filter/class.php
+++ local/components/impulsit/filter/class.php
@@ -11,20 +11,29 @@ class Filter extends CBitrixComponent{
     public function executeComponent()
     {
         global $jobsFilter;
+
         if(!empty($_GET['location'])){
             $jobsFilter['PROPERTY_LOCATION'] = $_GET['location'];
         }
 
         if(!empty($_GET['employment_rate'])){
-            $jobsFilter['EMPLOYMENT_RATE'] = $_GET['employment_rate'];
+            $jobsFilter['PROPERTY_EMPLOYMENT_RATE'] = $_GET['employment_rate'];
         }
 
         if(!empty($_GET['section'])){
-            $jobsFilter['SECTIONS'] = $_GET['section'];
+            $jobsFilter['PROPERTY_SECTIONS'] = $_GET['section'];
         }
 
         if(!empty($_GET['company'])){
-            $jobsFilter['COMPANY_NAME'] = $_GET['company'];
+            $jobsFilter['PROPERTY_COMPANY_NAME'] = $_GET['company'];
+        }
+
+        if(!empty($_GET['price_from'])){
+            $jobsFilter['>=PROPERTY_SALARY_FROM'] = $_GET['price_from'];
+        }
+
+        if(!empty($_GET['price_to'])){
+            $jobsFilter['<=PROPERTY_SALARY_UP_TO'] = $_GET['price_to'];
         }
 
         $this->arResult['SECTIONS'] = \Bitrix\Iblock\SectionTable::getList([
--- local/components/impulsit/filter/templates/.default/template.php
+++ local/components/impulsit/filter/templates/.default/template.php
@@ -11,77 +11,111 @@
                             <div class="row">
 <!--                                <div class="col-lg-12">-->
 <!--                                    <div class="single_field">-->
-<!--                                        <input type="text" placeholder="Поиск по ключевым словам">-->
+<!--                                        <input type="text" name="name" placeholder="Поиск" onchange="this.form.submit()">-->
 <!--                                    </div>-->
 <!--                                </div>-->
                                 <div class="col-lg-12">
                                     <div class="single_field">
-                                        <select name="location" class="wide">
+                                        <select name="location" class="wide" onchange="this.form.submit()">
                                             <option value="" data-display="Местоположение">Местоположение</option>
                                             <?php foreach ($arResult['LOCATIONS'] as $location): ?>
-                                                <option value="<?= $location['ID'] ?>"><?= $location['VALUE'] ?></option>
+                                                <option value="<?= $location['ID'] ?>"
+                                                    <?= isset($_GET['location']) && $_GET['location'] == $location['ID'] ? 'selected' : ''; ?>
+                                                >
+                                                    <?= $location['VALUE'] ?>
+                                                </option>
                                             <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-lg-12">
                                     <div class="single_field">
-                                        <select name="section" class="wide">
+                                        <select name="section" class="wide" onchange="this.form.submit()">
                                             <option value="" data-display="Категория">Категория</option>
                                             <?php foreach ($arResult['SECTIONS'] as $section): ?>
-                                                <option value="<?= $section['ID'] ?>"><?= $section['NAME'] ?></option>
+                                                <option value="<?= $section['ID'] ?>"
+                                                    <?= (isset($_GET['section']) && $_GET['section'] == $section['ID']) ? 'selected' : ''; ?>
+                                                >
+                                                    <?= $section['NAME'] ?>
+                                                </option>
                                             <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-lg-12">
                                     <div class="single_field">
-                                        <select name="employment_rate" class="wide">
+                                        <select name="employment_rate" class="wide" onchange="this.form.submit()">
                                             <option value="" data-display="Тип работы">Тип работы</option>
                                             <?php foreach ($arResult['EMPLOYMENT_RATES'] as $rate): ?>
-                                                <option value="<?= $rate['ID'] ?>"><?= $rate['VALUE'] ?></option>
+                                                <option value="<?= $rate['ID'] ?>"
+                                                    <?= isset($_GET['employment_rate']) && $_GET['employment_rate'] == $rate['ID'] ? 'selected' : ''; ?>
+                                                >
+                                                    <?= $rate['VALUE'] ?>
+                                                </option>
                                             <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-lg-12">
                                     <div class="single_field">
-                                        <select name="company" class="wide">
+                                        <select name="company" class="wide" onchange="this.form.submit()">
                                             <option value="" data-display="Компания">Компания</option>
                                             <?php foreach ($arResult['COMPANIES'] as $company): ?>
-                                                <option value="<?= $company['ID'] ?>"><?= $company['NAME'] ?></option>
+                                                <option value="<?= $company['ID'] ?>"
+                                                    <?= isset($_GET['company']) && $_GET['company'] == $company['ID'] ? 'selected' : ''; ?>
+                                                >
+                                                    <?= $company['NAME'] ?>
+                                                </option>
                                             <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
-                                <div class="col-lg-12">
-                                    <div class="single_field">
-                                        <div class="reset_btn">
-                                            <input id="filterBtn" style="padding: 13px 29px 13px 29px;" class="boxed-btn3 green w-100" type="submit" value="Отправить">
+
+                                <div style="margin-bottom: 30px;" class="range_wrap mt-3">
+                                    <label>Ценовой диапазон:</label>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="single_field">
+                                                <input type="text" name="price_from" class="form-control" placeholder="От"
+                                                       value="<?= (isset($_GET['price_from']) && is_numeric($_GET['price_from'])) ? $_GET['price_from'] : ''; ?>"
+                                                       onblur="document.getElementById('filterForm').submit()"
+                                                >
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="single_field">
+                                                <input type="text" name="price_to" class="form-control" placeholder="До"
+                                                       value="<?= (isset($_GET['price_to']) && is_numeric($_GET['price_to'])) ? $_GET['price_to'] : ''; ?>"
+                                                       onblur="document.getElementById('filterForm').submit()"
+                                                >
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+
+<!--                                <div class="col-lg-12">-->
+<!--                                    <div class="single_field">-->
+<!--                                        <div class="reset_btn">-->
+<!--                                            <input id="filterBtn" style="padding: 13px 29px 13px 29px;" class="boxed-btn3 green w-100" type="submit" value="Отправить">-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+
+                            </div>
+
+                            <div class="reset_btn">
+                                <button onclick="resetFilter()" class="boxed-btn3 red w-100" type="button">Сбросить</button>
                             </div>
 
                         </form>
                     </div>
-                    <div style="margin-bottom: 30px;" class="range_wrap mt-3">
-                        <label>Ценовой диапазон:</label>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="single_field">
-                                    <input type="text" name="price_from" class="form-control" placeholder="От">
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="single_field">
-                                    <input type="text" name="price_to" class="form-control" placeholder="До">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="reset_btn">
-                        <button class="boxed-btn3 red w-100" type="submit">Сбросить</button>
-                    </div>
+
                 </div>
             </div>
+
+            <script>
+                function resetFilter(){
+                    const url = window.location.pathname;
+                    window.location.href = url;
+                }
+            </script>
